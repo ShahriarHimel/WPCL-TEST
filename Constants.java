5e298306e99138762b0f7dd66c9b34902a02dd3a
ShahriarHimel <shahriarhimel20@gmail.com>
2025-03-04 01:47:44 +0600

    Replaced String Literals with Constants, Storing Those Constants in a New Class Called Constants.java.

---
+++ Constants.java
@@ -0,0 +1,11 @@
+// Constants.java
+
+public class Constants {
+
+    // Define the file path for the employee data file
+    public static final String EMPLOYEE_FILE_PATH = "employees.txt";
+
+    // Define the date format pattern (example: "dd-MM-yyyy")
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
+
+}
